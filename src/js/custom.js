d08fc4e27b1eda6092a6c0257fc67004ced4c920
Jason Climo <jason@Jasons-MBP.hub>
2020-08-10 14:44:09 +1200

    addded vue

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,17 +1,26 @@
 // All of the imports/requires
-var axios = require('axios');
+// var axios = require('axios');
 // A special/work-around kind of import/require for vue.js
 // Vue needs additional files and build tools so this way works
 // for our current gulp/webpack setup:
 import Vue from 'vue/dist/vue.js';
-import tippy from 'tippy.js';
-import anime from 'animejs/lib/anime.es.js';
-
-// Fonts
-
-
+// import tippy from 'tippy.js';
+// import anime from 'animejs/lib/anime.es.js';
 
 (function(){
-
+  var app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello Vue!',
+      increments: [0, 0, 0, 0]
+    },
+    methods: {
+      increment: function () {
+        var number = (app.increments[0] = app.increments[0] + 1);
+        app.increments[0] = number;
+        console.log(app.increments);
+      }
+    } // Methods ENDS
+  })
 
 })(); // iffe ENDS
